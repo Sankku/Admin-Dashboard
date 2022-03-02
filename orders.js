211eb257de5a126e3f20dcf60fc3671105d40148
Sankku <santiagoemprendedor9@gmail.com>
2022-03-02 19:04:08 -0300

    Upload app commit

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: "Coca Cola",
+    productNumber: "2212",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Pepsi",
+    productNumber: "1223",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Vodka",
+    productNumber: "523",
+    paymentStatus: "Due",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Wine",
+    productNumber: "4000",
+    paymentStatus: "Due",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Coca Cola",
+    productNumber: "2213",
+    paymentStatus: "Due",
+    shipping: "Declined",
+  },
+];
